a0eb432fe7b72ceff6a34f22332636828e9088d0
puscasDanut <danielionut@puscas.xyz>
2023-05-06 17:52:16 +0300

    Background pic

---
+++ main.py
@@ -0,0 +1,10 @@
+from turtle import Screen
+import turtle
+
+
+if __name__ == '__main__':
+    screen = Screen()
+    screen.setup(width=700, height=600)
+    screen.bgpic("Romania.gif")
+
+    screen.exitonclick()
